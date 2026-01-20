32758046dc5bed92faa5647065ac7d635639fe33
Shubham Yadav <sy978060@gmail.com>
2026-01-20 05:42:45 +0530

    frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ function App() {
       <Route
         path="/entry"
         element={
-          <ProtectedRoute>
+          
             <Entry />
-          </ProtectedRoute>
+          
         }
       />
 
