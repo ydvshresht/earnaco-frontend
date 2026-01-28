00293dc08bd598baa35b46d5c056b53f7a759c11
Shubham Yadav <sy978060@gmail.com>
2026-01-28 18:58:31 +0530

    Update frontend

--- src/pages/Entry.jsx
+++ src/pages/Entry.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../api/api";
 import "../styles/entry.css";
-
 import useProfile from "../hooks/useProfile";
 
 function Entry() {
@@ -35,7 +34,7 @@ function Entry() {
   }, []);
 
   return (
-    <div className="page">
+    <div className="screen">
       {/* HEADER */}
       <header>
         <div className="profile">
