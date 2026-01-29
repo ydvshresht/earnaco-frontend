f072e6e61491e3619ce6ad9bfb90d9223c3fc9f3
Shubham Yadav <sy978060@gmail.com>
2026-01-29 22:32:15 +0530

    Update frontend

--- src/pages/CreateTest.jsx
+++ src/pages/CreateTest.jsx
@@ -26,7 +26,7 @@ function CreateTest() {
       alert("Test created. Now add questions.");
 
       // 👉 Redirect to add/manage questions for this test
-      navigate(`/admin/manage-tests/${res.data._id}`);
+      navigate(`/admin/manage-questions/${res.data._id}`);
     } catch (err) {
       alert("Failed to create test");
     } finally {
