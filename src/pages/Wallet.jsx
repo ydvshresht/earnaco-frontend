5b64a6e153a9dc586590d67f359e83e267ad07e2
Shubham Yadav <sy978060@gmail.com>
2026-02-04 01:09:47 +0530

    Update frontend

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../api/api";
-import "../styles/wallet-ui.css";
+import "../styles/wallet.css";
 
 function Wallet() {
   const [coins, setCoins] = useState(0);
